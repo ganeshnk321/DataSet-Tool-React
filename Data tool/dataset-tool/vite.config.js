47532a12b2a70413b4bc2fc8880efe325879ae41
ganeshnk321 <ganunaik.32q@gmail.com>
2026-02-27 19:05:21 +0530

    Fix

--- Data tool/dataset-tool/vite.config.js
+++ Data tool/dataset-tool/vite.config.js
@@ -4,6 +4,7 @@ import path from "path"
 
 export default defineConfig({
   plugins: [react()],
+  base:process.env.VITE_BASE_PATH || "/DataSet-Tool-React",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
